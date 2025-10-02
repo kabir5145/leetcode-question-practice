e3bb4ef79dbb07e7bc6732ae87dd7df89ce8cb6b
kabir5145 <kabir10112006@gmail.com>
2025-10-02 21:04:06 +0530

    leetcode question 4 of array removing elements

--- com/company/array_question.java
+++ com/company/array_question.java
@@ -3,31 +3,22 @@ package com.company;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        if(nums.length==0) return 0;
-
-        int g = 0;
+class Solution_4 {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // index for the new array
         for (int i = 0; i < nums.length; i++) {
-                if (nums[i] != nums[g]) {
-                    nums[++g] = nums[i];
-                }
-        }return g+1;
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // keep only non-val elements
+                k++;
+            }
+        }
+        return k; // new length
     }
 }
 public class array_question {
     public static void main(String[] args) {
-        //Question 3: Given an integer array nums sorted in non-decreasing order,
-        // remove the duplicates in-place such that each unique element appears only once.
-        // The relative order of the elements should be kept the same.
-        // Then return the number of unique elements in nums.
-        Solution s = new Solution();
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};  // Example input
-
-        int newLength = s.removeDuplicates(nums);
-
-        // Print only the "new" array part (unique elements)
-        System.out.println("New length = " + newLength);
-        System.out.println("Unique elements = " + Arrays.toString(Arrays.copyOf(nums, newLength)));
+        //Question 4:Remove Element.
+        Solution_4 s = new Solution_4();
+        System.out.println(s.removeElement(new int[]{1, 2, 3, 2, 3, 2,},2));
     }
 }
