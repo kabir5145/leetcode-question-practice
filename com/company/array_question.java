7d0ceec2ddfd757c49d5160d9d36d3798f080037
kabir5145 <kabir10112006@gmail.com>
2025-10-10 22:24:15 +0530

    leetcode question 5 of array search insert

--- com/company/array_question.java
+++ com/company/array_question.java
@@ -1,24 +1,23 @@
 package com.company;
 
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution_4 {
-    public int removeElement(int[] nums, int val) {
-        int k = 0; // index for the new array
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i]; // keep only non-val elements
-                k++;
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        for(int i = 0;i<n;i++) {
+            if ( nums[i]>=target) {
+                return i;
             }
         }
-        return k; // new length
+        return n;
     }
 }
 public class array_question {
     public static void main(String[] args) {
-        //Question 4:Remove Element.
-        Solution_4 s = new Solution_4();
-        System.out.println(s.removeElement(new int[]{1, 2, 3, 2, 3, 2,},2));
+        Solution s = new Solution();
+        System.out.println(s.searchInsert(new int[]{1,2,3,4},8));
     }
 }
