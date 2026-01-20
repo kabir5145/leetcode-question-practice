ca5b442584bf9809695acfc160c54b37379784ab
kabir5145 <kabir10112006@gmail.com>
2026-01-20 11:23:31 +0530

    Valid Palindrome II

---
+++ com/company/Valid_Palindrome_II.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Valid_Palindrome_II {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Valid_Palindrome_II obj = new Valid_Palindrome_II();
+        System.out.println(obj.validPalindrome("abca"));
+    }
+}
