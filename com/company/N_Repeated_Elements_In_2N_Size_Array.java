27b398bd182e59248e256de369897de1ddb24479
kabir5145 <kabir10112006@gmail.com>
2026-01-21 21:46:28 +0530

    N-Repeated Element in Size 2N Array

---
+++ com/company/N_Repeated_Elements_In_2N_Size_Array.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class N_Repeated_Elements_In_2N_Size_Array {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length - 1;
+        if(nums[0] == nums[n]) return nums[0];
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) return nums[i];
+        }
+        return nums[0];
+    }
+    public static void main(String[] args) {
+
+    }
+}
