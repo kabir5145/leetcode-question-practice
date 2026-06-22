5307c45b0b9e767b942cf4a713e1e61d6949cfc9
Kabir5145 <kabir10112006@gmail.com>
2026-06-22 15:40:40 +0530

    Happy Number

--- com/company/Question_202.java
+++ com/company/Question_202.java
@@ -11,6 +11,7 @@ public class Question_202 {
 // Constant auxiliary space in practice (ignoring recursion stack),
 // otherwise O(k) for recursion depth.
 
+    /*
     public boolean isHappy(int n) {
         if(n == 1 || n == 7)return true;
 
@@ -24,6 +25,35 @@ public class Question_202 {
             }
             return isHappy(s);
         }
+    }
+     */
+
+    /*
+    Time : O(log n)
+    Space : O(1)
+     */
+    int func(int n){
+        int sum = 0;
+
+        while (n > 0) {
+            int d =  n % 10;
+            n /= 10;
+            sum += d*d;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+
+        while (fast != 1) {
+            slow = func(slow);
+            fast = func(func(fast));
+
+            if (slow == fast){
+                return slow == 1;
+            }
+        }
+        return true;
     }
     public static void main(String[] args) {
 //        Happy Number
